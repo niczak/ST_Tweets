c1af93e525369a81d777926ae751530e87e4d12e
Nicholas Kreidberg <nkreidberg@spydertrap.com>
2012-10-31 16:54:09 -0500

    Changed data feed to separate include file.

--- getTweets.php
+++ getTweets.php
@@ -22,7 +22,7 @@ foreach($a1[results] as $a) {
   if(!empty($a['entities']['media'][0]['media_url'])) {
     $html .= sprintf("<img src=\"%s\" /><br />\n", $a['entities']['media'][0]['media_url']);
   }
-  $html .= sprintf("<a href=\"http://www.twitter.com/%s\"><img src=\"%s\" /></a>\n", 
+  $html .= sprintf("<a href=\"http://www.twitter.com/%s\"><img style=\"width:48px; height:48px;\" src=\"%s\" /></a>\n", 
     $a['from_user'], $a['profile_image_url']);
   $html .= sprintf("%s", $a['text']);
   $html .= "</li>\n";
@@ -34,7 +34,7 @@ foreach($a2[results] as $a) {
   if(!empty($a['entities']['media'][0]['media_url'])) {
     $html .= sprintf("<img src=\"%s\" /><br />\n", $a['entities']['media'][0]['media_url']);
   }
-  $html .= sprintf("<a href=\"http://www.twitter.com/%s\"><img src=\"%s\" /></a>\n", 
+  $html .= sprintf("<a href=\"http://www.twitter.com/%s\"><img style=\"width:48px; height:48px;\" src=\"%s\" /></a>\n", 
     $a['from_user'], $a['profile_image_url']);
   $html .= sprintf("%s", $a['text']);
   $html .= "</li>\n";
@@ -46,14 +46,12 @@ foreach($a3[results] as $a) {
   if(!empty($a['entities']['media'][0]['media_url'])) {
     $html .= sprintf("<img src=\"%s\" /><br />\n", $a['entities']['media'][0]['media_url']);
   }
-  $html .= sprintf("<a href=\"http://www.twitter.com/%s\"><img src=\"%s\" /></a>\n", 
+  $html .= sprintf("<a href=\"http://www.twitter.com/%s\"><img style=\"width:48px; height:48px;\" src=\"%s\" /></a>\n", 
     $a['from_user'], $a['profile_image_url']);
   $html .= sprintf("%s\n", $a['text']);
   $html .= "</li>\n";
 }
 
 $html .= "</ul>\n";
-
-echo $html;
 
 ?>
